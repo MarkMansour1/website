0b709f684630ced9b812ec6d1651a012a4e0f047
MarkMansour1 <markmansour01@gmail.com>
2019-08-17 18:27:31 +0100

    footer

--- footer.php
+++ footer.php
@@ -2,19 +2,19 @@
 
   <div class="col-md-5 footertext">
     <img src="img/logo.png">
-    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <!-- <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> -->
   </div>
 
   <div class="col-md-4 text-right footertext">
     <br>
     <strong>Contact Info</strong>
-    <p>(+44) 444 4444<br>email@nuno.com</p>
-    <a href="#" target="_blank"><i class="fab fa-facebook-square"></i></a>
-    <a href="#" target="_blank"><i class="fab fa-twitter-square"></i></a>
-    <a href="#" target="_blank"><i class="fab fa-instagram"></i></a>
+    <p>(+44) 77699 73211<br>contact@markmansour.co.uk</p>
+    <a href="https://www.twitter.com/_markmansour" target="_blank"><i class="fab fa-twitter-square"></i></a>
+    <!-- <a href="#" target="_blank"><i class="fab fa-facebook-square"></i></a> -->
+    <!-- <a href="https://www.instagram.com/mark__mansour" target="_blank"><i class="fab fa-instagram"></i></a> -->
   </div>
 
   <hr class="socket">
-  <p>Copyright &copy; Mark Mansour <?php echo date('Y') ?></p>
+  <p>&copy; Copyright Mark Mansour <?php echo date('Y') ?></p>
 
 </div>
