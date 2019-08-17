3552bd3651e82f25f892ddbc5a139967336af130
MarkMansour1 <markmansour01@gmail.com>
2019-08-17 16:38:18 +0100

    Test

--- index.php
+++ index.php
@@ -85,7 +85,7 @@ include_once("php/dbconnect.php");
 		}
 		else {
 			echo '
-			<h1>Welcome</h1>
+			<h1>Welcome Mark</h1>
 			<h3>Custom web applications</h3>
 			<a href="login.php" class="btn btn-outline-light btn-lg">Log In</a>
 			';
